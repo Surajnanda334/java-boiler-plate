e80e9bb33bb615afb8653d81a6951a1c41bde1cd
Suraj Nanda <surajnanda334@gmail.com>
2025-04-04 14:11:35 +0530

    BOILER PLATE

---
+++ demo/src/test/java/sample/boilerPlate/BoilerPlateApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.boilerPlate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerPlateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
